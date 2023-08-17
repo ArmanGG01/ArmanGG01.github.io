b94033681fa3c4d2cd3048324e089b6901a92c5c
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-17 23:00:19 +0700

    Fix

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '1',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '1',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '1',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
